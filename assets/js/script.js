328397e615b166d4ce3789207d245025ba82d307
amanayak <amannayak2911@gmail.com>
2024-09-11 20:55:00 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+
+
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+    navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+    navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+    navLinks.classList.toggle("show1");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+    navLinks.classList.toggle("show3");
+}
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__content form", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".header__content .bar", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".header__image__card", {
+    duration: 1000,
+    interval: 500,
+    delay: 2500,
+});
+
+
+
+
